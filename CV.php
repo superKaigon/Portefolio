355d98d248b599408180a047dc2447e4e8397949
superKaigon <carlericeloy@gmail.com>
2017-05-09 14:02:50 +0200

    Add files via upload

---
+++ CV.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//FR">
+<html>
+	<head>
+	<?php
+		include ("header2.php")
+	?>	
+	</head>
+<body>
+	<h1 class = "center-align">Carl-Eric Eloy <sub>28 ans</sub>
+		<div>
+			 <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title center-align">Développeur Web</span>
+              <p>"L'inconscient est structuré comme un langage" J. Lacan</p>
+            </div>
+            <div class="card-action">
+              <li href="#" class="right-align">38 mail Gaston bardet 35650 Le Rheu</li>
+              <li href="#" class="left-align">06 81 26 09 81</li>
+              <li href="#" class="right-align">carlericeloy@gmail.com</li>
+              <li href="#" class="left-align">Permis B</li>
+            </div>
+          </div>
+        </div>
+      </div>
+		</div>	
+	</h1>
+	<section>
+
+		</section>
+		<footer>
+			
+		</footer>
+
+
+</body>
+</html>
---
+++ header2.php
@@ -0,0 +1,23 @@
+<head>
+	    <!--Import Google Icon Font-->
+	    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+		<!--Import jQuery before materialize.js-->
+	    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    
+	    <!--Let browser know website is optimized for mobile-->
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	    <!-- Initialise MDesign's Modal Component -->
+    	<script>
+    	  $(document).ready(function(){
+		    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+		    $('.modal').modal();
+		  });
+    	</script>
+    </head>
